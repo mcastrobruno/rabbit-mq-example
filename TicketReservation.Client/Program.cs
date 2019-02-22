3f14e25220ddff8b8b1d2a4de466ab1ac7c11793
Bruno Castro <bruno.castro@saipher.com.br>
2019-02-22 19:06:58 +0000

    Initial commit

---
+++ TicketReservation.Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketReservation.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Aguardando para inicializar....");
+            //Task.Delay(3000).Wait();
+
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumerHello = new EventingBasicConsumer(channel);
+                consumerHello.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [Consumer Hello] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumerHello);
+
+
+                var consumerTeste = new EventingBasicConsumer(channel);
+                consumerTeste.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [Consumer Teste] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: "teste",
+                                     autoAck: true,
+                                     consumer: consumerTeste);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
---
+++ TicketReservation.Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketReservation.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = string.Empty;
+
+            Console.WriteLine("Inicializando Server...");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                Console.WriteLine("Server inciado.");
+
+                Console.WriteLine("Inicializando Fila...");
+                channel.QueueDeclare(queue: "hello",
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+                Console.WriteLine($"Fila [hello] iniciada.");
+
+                channel.QueueDeclare(queue: "teste",
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+                Console.WriteLine($"Fila [teste] iniciada.");
+
+
+
+                while (result.ToUpper() != "E")
+                {
+                    Console.WriteLine("Press [E] to exit.");
+                    Console.WriteLine("Press [H] to send Message to Hello queue.");
+                    Console.WriteLine("Press [T] to send Message to Teste queue.");
+                    result = Console.ReadLine();
+
+                    if (result.ToUpper() == "E")
+                        break;
+                    else if (result.ToUpper() == "H")
+                    {
+                        SendMessage(channel, "hello");
+                    }
+                    else if (result.ToUpper() == "T")
+                    {
+                        SendMessage(channel, "teste");
+                    }
+                }
+            }
+        }
+
+
+        static void SendMessage(IModel channel, string queue)
+        {
+            string message = "Hello World";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                routingKey: queue,
+                basicProperties: null,
+                body: body);
+
+            Console.WriteLine($"[x] Sent {message} to {queue} queue");
+        }
+    }
+}
